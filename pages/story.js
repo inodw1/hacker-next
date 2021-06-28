9cd3ceca025007f5d1cb4934ba1fb2a6b124f276
inod-noetic <inod.wagachchi@wearenoetic.com>
2021-06-28 23:21:26 +0530

    Add styles to the story page :lipstick:

--- pages/story.js
+++ pages/story.js
@@ -23,15 +23,52 @@ class Story extends React.Component {
         
         return <Layout title={story.title}>
             <main>
-                 <h1>
+                 <h1 className="story-title">
                      <a href={story.url}>{story.title}</a>
                  </h1>
-                 <div>
+                 <div className="story-details">
                      <strong>{story.points} points</strong>
                      <strong>{story.comments_count} comments</strong>
                      <strong>{story.time_ago } points</strong>  
                  </div>
             </main>
+
+            <style jsx>{`
+                main {
+                    padding: 1em;
+                }
+
+                .story-title {
+                    font-size: 1.2rem;
+                    margin: 0;
+                    font-weight: 300;
+                    padding-bottom: 0.5em;
+                }
+
+                .story-title a {
+                    color: #333;
+                    text-decoration: none;
+                }
+
+                .story-title a:hover {
+                     text-decoration: underline;
+                }
+
+                .story-details {
+                    font-size: 0.8rem;
+                    padding-bottom: 1em;
+                    border-bottom: 1px solid rgba(0,0,0,0.1);
+                    margin-bottom: 1em;
+                }
+
+                .story-details strong {
+                    margin-right: 1em;
+                }
+
+                .story-details a {
+                     color: #f60;
+                }
+            `}</style>
         </Layout>
     }
 }
